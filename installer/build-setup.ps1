23ffd9dcc83ff25b1da8499ccccf9f66352dd129
dantwoashim <141327164+dantwoashim@users.noreply.github.com>
2026-09-23 14:17:11 +0545

    Bump product version to 0.1.10

--- installer/build-setup.ps1
+++ installer/build-setup.ps1
@@ -1,8 +1,8 @@
 param(
-  [string]$Version = '0.1.9',
+  [string]$Version = '0.1.10',
   [string]$MsiPath,
   [string]$Output = (Join-Path (Split-Path -Parent $PSScriptRoot) 'dist-dev\installer'),
-  [string]$Tag = 'v0.1.9-preview'
+  [string]$Tag = 'v0.1.10-preview'
 )
 
 $ErrorActionPreference = 'Stop'
--- installer/build.ps1
+++ installer/build.ps1
@@ -3,7 +3,7 @@ param(
   [string]$Configuration = 'release',
   [string]$BinDir,
   [string]$UiDir,
-  [string]$Version = '0.1.9',
+  [string]$Version = '0.1.10',
   [Parameter(Mandatory = $true)]
   [string]$DriveClientCredentials,
   [long]$SourceDateEpoch = 946684800,
